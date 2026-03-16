a89df27edafc335fc3578fcc666d8fa12be9d181
Alexander Matveenkov <amat.andre@gmail.com>
2026-03-17 01:37:30 +0300

    fix first commit

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,25 @@
 package service
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+var errInput = errors.New("input value error")
+
+func DefinitionAndConversion (input string) (string, error) {
+	if input == "" {
+		return "", errInput
+	}
+	
+	symbols := "-. "
+	if strings.Trim(input, symbols) != "" {
+		output := morse.ToMorse(input)
+		return output, nil
+	}
+
+	output := morse.ToText(input)
+	return output, nil
+}
